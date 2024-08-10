52d3c3f0baa4eaa0b3424f653ccceec5efb31da9
Isutomu <isutomu@gmail.com>
2024-08-10 18:40:46 -0300

    :card_file_box: raw: add a section data.

---
+++ data/data.js
@@ -0,0 +1,11 @@
+export const sectionsData = [
+  {
+    name: "General info",
+    fields: {
+      name: "text",
+      age: "text",
+      email: "email",
+      phone: "number",
+    },
+  },
+];
